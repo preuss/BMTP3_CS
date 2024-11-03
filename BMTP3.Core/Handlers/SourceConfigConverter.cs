0bb1bef19e01f175cff3e3bd7c2258e6486ade25
Overwatch <preuss@users.noreply.github.com>
2024-11-03 20:14:24 +0100

    Refactor, move class to files

---
+++ BMTP3.Core/Handlers/SourceConfigConverter.cs
@@ -0,0 +1,48 @@
+﻿using BMTP3.Core.BackupSource;
+using BMTP3.Core.Configs;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMTP3.Core.Handlers {
+	public class SourceConfigConverter : JsonConverter<ISourceConfig?> {
+		public override ISourceConfig? ReadJson(JsonReader reader, Type objectType, ISourceConfig? existingValue, bool hasExistingValue, JsonSerializer serializer) {
+			JObject jo = JObject.Load(reader);
+			SourceType? sourceType = jo["SourceType"]?.ToObject<SourceType>(serializer); // Uses SourceTypeConverter for ReadJson
+
+			if(sourceType == null) {
+				throw new ArgumentException("Invalid source type", nameof(sourceType));
+			}
+
+			switch(sourceType) {
+				case SourceType.Drive:
+					return jo.ToObject<DriveSourceConfig>();
+				case SourceType.Device:
+					return jo.ToObject<DeviceSourceConfig>();
+				default:
+					throw new ArgumentException("Invalid source type", nameof(sourceType));
+			}
+		}
+		public override void WriteJson(JsonWriter writer, ISourceConfig? value, JsonSerializer serializer) {
+			if(value == null) {
+				// Handle the case when value is null
+				// For example, you can throw an exception or write a default value
+				throw new ArgumentNullException(nameof(value));
+			}
+
+			JObject jo = JObject.FromObject(value);
+
+			// Check if the property already exists
+			jo.Remove("SourceType");
+			if(!jo.ContainsKey("SourceType")) {
+				jo.Add("SourceType", JToken.FromObject(value.SourceType.ToString()));
+			}
+
+			jo.WriteTo(writer);
+		}
+	}
+}
---
+++ BMTP3.Core/Handlers/SourceTypeConverter.cs
@@ -0,0 +1,27 @@
+﻿using BMTP3.Core.BackupSource;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMTP3.Core.Handlers {
+	public class SourceTypeConverter : JsonConverter<SourceType> {
+		public override SourceType ReadJson(JsonReader reader, Type objectType, SourceType existingValue, bool hasExistingValue, JsonSerializer serializer) {
+			JToken token = JToken.Load(reader);
+			if(token.Type == JTokenType.String && token.Value<string>() is string value) {
+				return (SourceType)Enum.Parse(typeof(SourceType), value);
+			} else {
+				throw new Exception("Invalid JSON value for SourceType");
+			}
+		}
+		public override void WriteJson(JsonWriter writer, SourceType value, JsonSerializer serializer) {
+			// Not really used
+			// TODO: Find out why not used, and why this code has to be added to SourceConfigConverter.WriteJson
+			// TODO: Find out how to use this in SourceConfigConverter
+			writer.WriteValue(value.ToString());
+		}
+	}
+}
