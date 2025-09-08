626a64c74598a103af88c02b95bc55e5f551a5f4
Overwatch <preuss@users.noreply.github.com>
2025-09-08 10:46:24 +0200

    Formatting

--- BMTP3.Core/BackupSource/Drives/DriveBackupJob.cs
+++ BMTP3.Core/BackupSource/Drives/DriveBackupJob.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 
 namespace BMTP3.Core.BackupSource.Drives {
 	[DebuggerDisplay("DriveInfo: {DriveInfo.Name}, DriveSourceConfig:{DriveSourceConfig.Title}")]
-	public class DriveBackupJob : BackupJob{
-		public DriveBackupJob(DriveInfo driveInfo, DriveSourceConfig driveSourceConfig): base(driveSourceConfig) {
+	public class DriveBackupJob : BackupJob {
+		public DriveBackupJob(DriveInfo driveInfo, DriveSourceConfig driveSourceConfig) : base(driveSourceConfig) {
 			DriveInfo = driveInfo;
 		}
 		public DriveInfo DriveInfo { get; }
