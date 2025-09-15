e120283f52605c36b0dd265680c941be013af547
Overwatch <preuss@users.noreply.github.com>
2025-09-15 14:16:28 +0200

    Added SharpHash

---
+++ BMTP3.Core/Handlers/Crypto/SharpHashSHA3_512_Keccak.cs
@@ -0,0 +1,34 @@
+﻿using Blake3;
+using SharpHash;
+using SharpHash.Base;
+using SharpHash.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMTP3.Core.Handlers.Crypto {
+	public class SharpHashSHA3_512_Keccak : HashAlgorithm {
+		private readonly IHash _hash;
+
+		public SharpHashSHA3_512_Keccak() {
+			_hash = HashFactory.Crypto.CreateKeccak_512();
+			HashSizeValue = _hash.HashSize;
+			Initialize();
+		}
+
+		public override void Initialize() {
+			_hash.Initialize();
+		}
+
+		protected override void HashCore(byte[] array, int ibStart, int cbSize) {
+			_hash.TransformBytes(array, ibStart, cbSize);
+		}
+
+		protected override byte[] HashFinal() {
+			return _hash.TransformFinal().GetBytes();
+		}
+	}
+}
