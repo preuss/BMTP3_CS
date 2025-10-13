0c0a63cff221e9d9fb31d72cd5471e9a5c51beed
Overwatch <preuss@users.noreply.github.com>
2025-10-14 01:24:03 +0200

    Update

--- BMTP3.Consoles/ConsoleCommands/BaseOptionsModel.cs
+++ BMTP3.Consoles/ConsoleCommands/BaseOptionsModel.cs
@@ -3,133 +3,158 @@ using System.Collections.Generic;
 using System.CommandLine;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BMTP3.Consoles.ConsoleCommands;
 public abstract class BaseOptionsModel
 {
-	public virtual void PopulateFromParseResult(ParseResult parseResult)
-	{
-		var type = GetType();
-		var staticProps = type.GetProperties(BindingFlags.Public | BindingFlags.Static);
+    public virtual void PopulateFromParseResult(ParseResult parseResult)
+    {
+        var type = GetType();
+        var staticProps = type.GetProperties(BindingFlags.Public | BindingFlags.Static);
 
-		// Track Parse{Name}Option
-		HashSet<string> assigned = new(StringComparer.Ordinal);
+        // Validate all Parse{Name}Option delegates
+        foreach (var parseProp in staticProps.Where(p => p.Name.StartsWith("Parse") && p.Name.EndsWith("Option")))
+        {
+            var baseName = parseProp.Name.Substring("Parse".Length, parseProp.Name.Length - "Parse".Length - "Option".Length);
 
-		foreach(var parseProp in staticProps.Where(p => p.Name.StartsWith("Parse") && p.Name.EndsWith("Option")))
-		{
-			var baseName = parseProp.Name.Substring("Parse".Length, parseProp.Name.Length - "Parse".Length - "Option".Length);
+            var instanceProp = type.GetProperty(baseName, BindingFlags.Public | BindingFlags.Instance);
+            if (instanceProp == null)
+            {
+                throw new InvalidOperationException(
+                    $"Parse delegate '{parseProp.Name}' exists, but there is no instance property '{baseName}' on type '{type.FullName}'. " +
+                    $"Add a property '{baseName}' or remove '{parseProp.Name}'.");
+            }
 
-			var instanceProp = type.GetProperty(baseName, BindingFlags.Public | BindingFlags.Instance);
-			if(instanceProp == null || !instanceProp.CanWrite)
-			{
-				continue;
-			}
+            if (!instanceProp.CanWrite)
+            {
+                throw new InvalidOperationException(
+                    $"Property '{baseName}' on '{type.FullName}' is not writable, but '{parseProp.Name}' is declared.");
+            }
 
-			if(parseProp.GetValue(null) is not Delegate parserFunc)
-			{
-				continue;
-			}
+            if (parseProp.GetValue(null) is not Delegate parserFunc)
+            {
+                throw new InvalidOperationException(
+                    $"Parse delegate property '{parseProp.Name}' on '{type.FullName}' is null or not a delegate.");
+            }
 
-			// Validation: Return type from parserFunc need to match property type
-			var parserReturnType = parserFunc.GetType().GetMethod("Invoke")?.ReturnType;
-			var propertyType = instanceProp.PropertyType;
-			if (parserReturnType is not null && !propertyType.IsAssignableFrom(parserReturnType))
-			{
-				throw new InvalidOperationException(
-					$"Type mismatch: Parse{baseName}Option returns {parserReturnType.Name}, " +
-					$"but property '{baseName}' er is of type {propertyType.Name} i {type.Name}.");
-			}
+            var parserReturnType = parserFunc.GetType().GetMethod("Invoke")?.ReturnType;
+            var propertyType = instanceProp.PropertyType;
+            if (parserReturnType is not null && !propertyType.IsAssignableFrom(parserReturnType))
+            {
+                throw new InvalidOperationException(
+                    $"Type mismatch: '{parseProp.Name}' returns {parserReturnType.FullName}, " +
+                    $"but property '{baseName}' is of type {propertyType.FullName} in {type.FullName}.");
+            }
+        }
 
-			object? value = parserFunc.DynamicInvoke(parseResult);
-			if(value != null)
-			{
-				instanceProp.SetValue(this, value);
-				assigned.Add(baseName);
-			}
-		}
+        // Track Parse{Name}Option assignments
+        HashSet<string> assigned = new(StringComparer.Ordinal);
 
-		foreach(var optionProp in staticProps.Where(p => typeof(Option).IsAssignableFrom(p.PropertyType)))
-		{
-			// Is {name}Option -> {name}
-			string baseName = optionProp.Name.EndsWith("Option", StringComparison.Ordinal)
-				? optionProp.Name[..^"Option".Length]
-				: optionProp.Name;
+        foreach (var parseProp in staticProps.Where(p => p.Name.StartsWith("Parse") && p.Name.EndsWith("Option")))
+        {
+            var baseName = parseProp.Name.Substring("Parse".Length, parseProp.Name.Length - "Parse".Length - "Option".Length);
 
-			if (assigned.Contains(baseName))
-			{
-				// Already delegated to Parse{Name}Option
-				continue;
-			}
+            var instanceProp = type.GetProperty(baseName, BindingFlags.Public | BindingFlags.Instance);
+            if (instanceProp == null || !instanceProp.CanWrite)
+            {
+                continue;
+            }
 
-			var instanceProp = type.GetProperty(baseName, BindingFlags.Public | BindingFlags.Instance);
-			if(instanceProp == null || !instanceProp.CanWrite)
-			{
-				continue;
-			}
+            if (parseProp.GetValue(null) is not Delegate parserFunc)
+            {
+                continue;
+            }
 
-			if(optionProp.GetValue(null) is not Option opt)
-			{
-				continue;
-			}
+            object? value = parserFunc.DynamicInvoke(parseResult);
+            if (value != null)
+            {
+                instanceProp.SetValue(this, value);
+                assigned.Add(baseName);
+            }
+        }
 
-			object? value = GetOptionValue(parseResult, opt, instanceProp.PropertyType);
-			if(value != null)
-			{
-				instanceProp.SetValue(this, value);
-			}
-		}
-	}
-	private static object? GetOptionValue(ParseResult parseResult, Option opt, Type targetType)
-	{
-		var optionType = typeof(Option<>).MakeGenericType(targetType);
-		if (!optionType.IsInstanceOfType(opt))
-		{
-			throw new InvalidOperationException($"Option instance is not of type Option<{targetType.Name}>. Actual type: {opt.GetType()}");
-		}
-		var method = typeof(ParseResult)
-			.GetMethods()
-			.Where(m =>
-				m.Name == nameof(ParseResult.GetValue) &&
-				m.IsGenericMethod &&
-				m.GetParameters().Length == 1
-		    )
-			.FirstOrDefault(m => 
-				m.GetParameters()[0].ParameterType.IsGenericType &&
-				m.GetParameters()[0].ParameterType.GetGenericTypeDefinition() == typeof(Option<>)
-			);
+        foreach (var optionProp in staticProps.Where(p => typeof(Option).IsAssignableFrom(p.PropertyType)))
+        {
+            // E.g. VerboseOption -> Verbose
+            string baseName = optionProp.Name.EndsWith("Option", StringComparison.Ordinal)
+                ? optionProp.Name[..^"Option".Length]
+                : optionProp.Name;
 
-		if(method != null)
-		{
-			var generic = method.MakeGenericMethod(targetType);
-			object castedOpt = Convert.ChangeType(opt, optionType);
-			return generic.Invoke(parseResult, new object[] { castedOpt });
-		}
-		return null;
-	}
-	public List<Option> GetAllOptions()
-	{
-		var type = GetType();
-		return type
-			.GetProperties(BindingFlags.Public | BindingFlags.Static)
-			.Where(p => typeof(Option).IsAssignableFrom(p.PropertyType))
-			.Select(p => p.GetValue(null))
-			.OfType<Option>()
-			.ToList();
-	}
+            if (assigned.Contains(baseName))
+            {
+                // Already delegated to Parse{Name}Option
+                continue;
+            }
 
-	public IEnumerable<Delegate> GetAllParseOptions()
-	{
-		var type = GetType();
-		return type
-			.GetProperties(BindingFlags.Public | BindingFlags.Static)
-			.Where(p => 
-				p.Name.StartsWith("Parse") && 
-				p.Name.EndsWith("Option") && 
-				typeof(Delegate).IsAssignableFrom(p.PropertyType)
-			)
-			.Select(p => p.GetValue(null))
-			.OfType<Delegate>();
-	}
+            var instanceProp = type.GetProperty(baseName, BindingFlags.Public | BindingFlags.Instance);
+            if (instanceProp == null || !instanceProp.CanWrite)
+            {
+                continue;
+            }
+
+            if (optionProp.GetValue(null) is not Option opt)
+            {
+                continue;
+            }
+
+            object? value = GetOptionValue(parseResult, opt, instanceProp.PropertyType);
+            if (value != null)
+            {
+                instanceProp.SetValue(this, value);
+            }
+        }
+    }
+
+    private static object? GetOptionValue(ParseResult parseResult, Option opt, Type targetType)
+    {
+        var optionType = typeof(Option<>).MakeGenericType(targetType);
+        if (!optionType.IsInstanceOfType(opt))
+        {
+            throw new InvalidOperationException($"Option instance is not of type Option<{targetType.Name}>. Actual type: {opt.GetType()}");
+        }
+        var method = typeof(ParseResult)
+            .GetMethods()
+            .Where(m =>
+                m.Name == nameof(ParseResult.GetValue) &&
+                m.IsGenericMethod &&
+                m.GetParameters().Length == 1
+            )
+            .FirstOrDefault(m =>
+                m.GetParameters()[0].ParameterType.IsGenericType &&
+                m.GetParameters()[0].ParameterType.GetGenericTypeDefinition() == typeof(Option<>)
+            );
+
+        if (method != null)
+        {
+            var generic = method.MakeGenericMethod(targetType);
+            object castedOpt = Convert.ChangeType(opt, optionType);
+            return generic.Invoke(parseResult, new object[] { castedOpt });
+        }
+        return null;
+    }
+
+    public List<Option> GetAllOptions()
+    {
+        var type = GetType();
+        return type
+            .GetProperties(BindingFlags.Public | BindingFlags.Static)
+            .Where(p => typeof(Option).IsAssignableFrom(p.PropertyType))
+            .Select(p => p.GetValue(null))
+            .OfType<Option>()
+            .ToList();
+    }
+
+    public IEnumerable<Delegate> GetAllParseOptions()
+    {
+        var type = GetType();
+        return type
+            .GetProperties(BindingFlags.Public | BindingFlags.Static)
+            .Where(p =>
+                p.Name.StartsWith("Parse") &&
+                p.Name.EndsWith("Option") &&
+                typeof(Delegate).IsAssignableFrom(p.PropertyType)
+            )
+            .Select(p => p.GetValue(null))
+            .OfType<Delegate>();
+    }
 }
