6930efc533a4fafd46cab91d484d3908f5bbe457
Overwatch <preuss@users.noreply.github.com>
2025-11-23 17:20:04 +0100

    Update

---
+++ BMTP3.Consoles/Services/ConsolesPrinter.cs
@@ -0,0 +1,28 @@
+﻿using BMTP3.Consoles.ConsoleCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMTP3.Consoles.Services;
+public class ConsolesPrinter
+{
+	/// <summary>
+	/// Prints the options model to the console.
+	/// </summary>
+	/// <param name="optionsModels">The options models to print.</param>
+	public void PrintOptionsModel(params BaseOptionsModel[] optionsModels)
+	{
+		foreach(var optionsModel in optionsModels)
+		{
+			string header = $"{optionsModel.GetType().Name}:";
+			Console.WriteLine(header);
+			Console.WriteLine(new string('=', header.Length));
+			foreach(var line in optionsModel.GetOptionPropertyValues())
+			{
+				Console.WriteLine("  " + line);
+			}
+		}
+	}
+}
---
+++ BMTP3.Consoles/Startup/Configurations/ConsolesServiceSetup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMTP3.Consoles.Services;
+
+namespace BMTP3.Consoles.Startup.Configurations;
+public class ConsolesServiceSetup : IServiceSetup
+{
+	public void Configure(IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddSingleton<ConsolesPrinter>();
+	}
+}
