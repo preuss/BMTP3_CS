fd3a7e067f163a692d9464b9a3bc80164610935b
Overwatch <preuss@users.noreply.github.com>
2025-11-25 00:04:16 +0100

    Added Coordinator class for increment

---
+++ BMTP3.Core2/BackupNew/Events/TraversalProgressCounter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace BMTP3.Core2.BackupNew.Events;
+public class TraversalProgressCounter
+{
+	private int _fileCount = 0;
+	private int _directoryCount = 0;
+	private readonly object _lock = new();
+
+	private readonly SynchronizationContext _syncContext;
+
+	public event EventHandler<int>? FileCountChanged;
+	public event EventHandler<int>? DirectoryCountChanged;
+	public event EventHandler<FileAndDirectoryCount>? CombinedCountChanged;
+
+	public TraversalProgressCounter()
+	{
+		_syncContext = SynchronizationContext.Current ?? new SynchronizationContext();
+	}
+
+	public void IncrementFileCount(int incrementWith = 1)
+	{
+		if (incrementWith == 0) return;
+		FileAndDirectoryCount currentCount;
+		lock (_lock)
+		{
+			_fileCount += incrementWith;
+			currentCount = new (_fileCount, _directoryCount);
+		}
+		PostEvent(FileCountChanged, currentCount.FileCount);
+		PostEvent(CombinedCountChanged, currentCount);
+	}
+
+	public void IncrementDirectoryCount(int incrementWith = 1)
+	{
+		if (incrementWith == 0) return;
+		FileAndDirectoryCount currentCount;
+		lock (_lock)
+		{
+			_directoryCount += incrementWith;
+			currentCount = new (_fileCount, _directoryCount);
+		}
+		PostEvent(DirectoryCountChanged, currentCount.DirectoryCount);
+		PostEvent(CombinedCountChanged, currentCount);
+	}
+
+	private void PostEvent<T>(EventHandler<T>? handler, T value)
+	{
+		if (handler != null)
+		{
+			_syncContext.Post(_ => handler(this, value), null);
+		}
+	}
+}
+public record FileAndDirectoryCount(int FileCount, int DirectoryCount);
