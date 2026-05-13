df00625e3902f6b2932b7ef605195931e047c0a3
Overwatch <preuss@users.noreply.github.com>
2026-05-13 12:59:22 +0200

    Some update

---
+++ BMTP3.Core4/InternalsVisibleTo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("BMTP3.Core4.Tests")]
+
+namespace BMTP3.Core4;
+
+internal class InternalsVisibleTo
+{
+}
