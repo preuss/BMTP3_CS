38fbfb6c1db6ba9c995d27fa42300cca38240218
Overwatch <preuss@users.noreply.github.com>
2026-06-07 22:46:38 +0200

    Temp added connection

---
+++ BMTP3.Core4/Storage/ConnectedFileSystemSource.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMTP3.Core4.Storage;
+
+internal sealed class ConnectedFileSystemSource : IConnectedFileSystemSource
+{
+	private readonly IBackupFileSystemDriveInfo _fileSystemDriveInfo;
+	private readonly DriveInfo _drive;
+
+	private bool _disposed;
+
+	public ConnectedFileSystemSource(IBackupFileSystemDriveInfo fileSystemDriveInfo, DriveInfo drive)
+	{
+		_fileSystemDriveInfo = fileSystemDriveInfo ?? throw new ArgumentNullException(nameof(fileSystemDriveInfo));
+		_drive = drive ?? throw new ArgumentNullException(nameof(drive));
+	}
+
+	public IBackupDriveInfo DriveInfo => _fileSystemDriveInfo;
+
+	public IBackupFileSystemDriveInfo FileSystemDriveInfo => _fileSystemDriveInfo;
+
+	public DriveInfo Drive
+	{
+		get
+		{
+			ThrowIfDisposed();
+			return _drive;
+		}
+	}
+
+	public void Dispose()
+	{
+		_disposed = true;
+	}
+
+	private void ThrowIfDisposed()
+	{
+		if(_disposed)
+			throw new ObjectDisposedException(nameof(ConnectedFileSystemSource));
+	}
+}
---
+++ BMTP3.Core4/Storage/ConnectedMediaDeviceSource.cs
@@ -0,0 +1,47 @@
+﻿using BMTP3.Core4.Devices;
+
+namespace BMTP3.Core4.Storage;
+
+internal sealed class ConnectedMediaDeviceSource : IConnectedMediaDeviceSource
+{
+	private readonly IBackupMediaDriveInfo _mediaDriveInfo;
+	private readonly IMediaDevice _device;
+
+	private bool _disposed;
+
+	public ConnectedMediaDeviceSource(IBackupMediaDriveInfo mediaDriveInfo, IMediaDevice device)
+	{
+		_mediaDriveInfo = mediaDriveInfo ?? throw new ArgumentNullException(nameof(mediaDriveInfo));
+		_device = device ?? throw new ArgumentNullException(nameof(device));
+	}
+
+	public IBackupDriveInfo DriveInfo => _mediaDriveInfo;
+
+	public IBackupMediaDriveInfo MediaDriveInfo => _mediaDriveInfo;
+
+	public IMediaDevice Device
+	{
+		get
+		{
+			ThrowIfDisposed();
+			return _device;
+		}
+	}
+
+	public void Dispose()
+	{
+		if(_disposed)
+			return;
+
+		_device.Disconnect();
+		_device.Dispose();
+
+		_disposed = true;
+	}
+
+	private void ThrowIfDisposed()
+	{
+		if(_disposed)
+			throw new ObjectDisposedException(nameof(ConnectedMediaDeviceSource));
+	}
+}
